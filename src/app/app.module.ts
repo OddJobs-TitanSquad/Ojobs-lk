86504d6915b0aaefff8ed06b98ee2d1e24440543
lahirusudesh <164189x@uom.lk>
2018-10-09 16:44:41 +0530

    post-job Component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import{RouterModule,Routes} from '@angular/router';
-import{UserService} from './user.service';
-import{JobService} from './job.service';
+import { RouterModule,Routes } from '@angular/router';
+import { UserService } from './user.service';
+import { JobService } from './job.service';
 import { MembershipsComponent } from './memberships/memberships.component';
 import { CompletedJobsComponent } from './completed-jobs/completed-jobs.component';
 import { ComplainComponent } from './complain/complain.component';
@@ -14,6 +14,7 @@ import { ComplainComponent } from './complain/complain.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PostJobComponent } from './post-job/post-job.component';
 
 
 const appRoutes:Routes=[
@@ -41,6 +42,8 @@ const appRoutes:Routes=[
     HomeComponent,
     
     LoginComponent,
+    
+    PostJobComponent,
     
     //ProfileComponent
   ],
